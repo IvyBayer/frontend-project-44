9b2f2c22b12397a9af1351c550f8365cac9a3546
Polina Ivanova <103686646+IvyBayer@users.noreply.github.com>
2023-01-16 18:21:08 +0300

    add

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const userName = () => {
+    const name = readlineSync.question(`Welcome to the Brain Games!
+May I have your name? `);
+    return `Hello ${name}`;
+};
+
+export default userName;
