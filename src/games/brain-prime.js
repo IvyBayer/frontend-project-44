83213c70ba244e7d5ffcb9d95ac250cfe83fe3b5
Polina Ivanova <103686646+IvyBayer@users.noreply.github.com>
2023-01-21 18:35:38 +0300

    Update brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,9 +3,9 @@ import games from '../index.js';
 
 const brainPrimeGames = () => {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+  const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53];
   const round = () => {
-    const getrandomNumber = _.random(1, 100);
+    const getrandomNumber = _.random(1, 58);
     if (primeNumbers.includes(getrandomNumber) === true) {
       return [getrandomNumber, 'yes'];
     }
