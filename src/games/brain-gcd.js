237d21d1b96d2dfb87dd5eccdca13c0fb927325e
Polina Ivanova <103686646+IvyBayer@users.noreply.github.com>
2023-01-21 18:47:16 +0300

    Update brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,23 +4,15 @@ import games from '../index.js';
 const brainGcdGames = () => {
   const rules = 'Find the greatest common divisor of given numbers.';
   const nod = (randomNumber1, rangomNumber2) => {
-    let number1 = randomNumber1;
-    let number2 = rangomNumber2;
+    const number1 = randomNumber1;
+    const number2 = rangomNumber2;
     if (number2 > number1) {
-      const temp = number1;
-      number1 = number2;
-      number2 = temp;
+      return nod(number2, number1);
     }
-    while (true) {
-      if (number2 === 0) {
-        return number1;
-      }
-      number1 %= number2;
-      if (number1 === 0) {
-        return number2;
-      }
-      number2 %= number1;
+    if (!number2) {
+      return number1;
     }
+    return nod(number2, number1 % number2);
   };
   const round = () => {
     const getrandomNumber1 = _.random(1, 100);
